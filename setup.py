2b36fae677ada91a43c505157fd7af214eae7719
Julia310 <JPeters92@protonmail.com>
2024-03-25 20:35:00 +0100

    enable conda package creation using miniconda

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='mltools',
     version='0.1.0',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    package_dir={'': 'mltools'},
+    packages=find_packages(where='mltools'),
     install_requires=[
         'bokeh >=2.4.3, <3',
         'dask >2024.2',
