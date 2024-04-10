66d6ae49de190ef12cc9226bcd52f1f16e1c9db9
Julia310 <JPeters92@protonmail.com>
2024-04-10 14:33:46 +0200

    increase mltools version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mltools',
-    version='0.1.0',
+    version='0.2.0',
     package_dir={'': 'mltools'},
     packages=find_packages(where='mltools'),
     install_requires=[
