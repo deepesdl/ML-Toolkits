28747984c666d1a9c2e47ed4c0df3c3082a3dea1
Julia310 <JPeters92@protonmail.com>
2024-05-01 16:33:44 +0200

    adapt method to enable computations on fully loaded xcubes

--- mltools/mltools/statistics.py
+++ mltools/mltools/statistics.py
@@ -5,21 +5,24 @@ import xarray as xr
 def get_statistics(ds: xr.Dataset, var: str):
     """returns mean and std values of variable var of xarray ds"""
     data_var = ds[var]
-
-    mean = data_var.mean().compute().values.item()
-    std = data_var.std().compute().values.item()
+    if isinstance(data_var, np.ndarray):
+        mean = data_var.mean().item()
+        std = data_var.std().item()
+    else:
+        mean = data_var.mean().compute().values.item()
+        std = data_var.std().compute().values.item()
 
     return [mean, std]
 
 
 def normalize(x: np.ndarray, xmin: float, xmax: float):
     """min-max feature scaling of x, shift values to range [0,1]"""
-    return (x - xmin ) / (xmax - xmin)
+    return (x - xmin) / (xmax - xmin)
 
 
 def standardize(x: np.ndarray, xmean: float, xstd: float):
     """transforms distribution to mean 0 and variance 1"""
-    return (x - xmean ) / xstd
+    return (x - xmean) / xstd
 
 
 def undo_normalizing(x: np.ndarray, xmin: float, xmax: float):
@@ -34,6 +37,11 @@ def undo_standardizing(x: np.ndarray, xmean: float, xstd: float):
 
 def get_range(ds: xr.Dataset, var: str):
     """returns min and max values of variable var of xarray ds"""
-    x = ds[var].min().values
-    y = ds[var].max().values
-    return [x,y]
+    data_var = ds[var]
+    if isinstance(data_var, np.ndarray):
+        min = data_var.min().item()
+        max = data_var.max().item()
+    else:
+        min = data_var.min().values
+        max = data_var.max().values
+    return [min, max]
