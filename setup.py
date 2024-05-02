7087b63e7fadad427f5b559d6587cc3683e1c24c
Julia310 <JPeters92@protonmail.com>
2024-05-03 01:37:55 +0200

    add gap filling helper to package creation

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mltools',
-    version='0.2.0',
+    name='ml4xcube',
+    version='0.0.4',
     package_dir={'': 'mltools'},
     packages=find_packages(where='mltools'),
+    package_data={
+        'ml4xcube': ['gapfilling/helper/*'],  # Include all files within the 'helper' directory
+    },
     install_requires=[
-        'bokeh >=2.4.3, <3',
-        'dask >2024.2',
-        'dask-core >2024.2',
+        'bokeh >=2.4.3',
+        'dask >=2023.2.0',
         'jinja2 ==3.1.3',
         'mypy_extensions ==1.0.0',
         'numpy >=1.24',
         'pandas >=2.2',
         'scikit-learn >1.3.1',
-        'xarray >=2024.0',
+        'xarray >2023.8.0',
         'zarr >2.11',
         'rechunker >=0.5.1',
-        'sentinelhub'
+        'sentinelhub',
+        'xcube'
     ],
 )
