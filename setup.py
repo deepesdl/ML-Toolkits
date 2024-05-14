2808bc959db9b8140511adf243c404445a2f7160
Julia310 <JPeters92@protonmail.com>
2024-05-14 16:46:48 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml4xcube',
-    version='0.0.4',
+    version='0.0.6',
     package_dir={'': 'mltools'},
     packages=find_packages(where='mltools'),
     package_data={
@@ -11,15 +11,12 @@ setup(
     install_requires=[
         'bokeh >=2.4.3',
         'dask >=2023.2.0',
-        'jinja2 ==3.1.3',
-        'mypy_extensions ==1.0.0',
         'numpy >=1.24',
         'pandas >=2.2',
         'scikit-learn >1.3.1',
         'xarray >2023.8.0',
         'zarr >2.11',
         'rechunker >=0.5.1',
-        'sentinelhub',
-        'xcube'
+        'sentinelhub'
     ],
 )
