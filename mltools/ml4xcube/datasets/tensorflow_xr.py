0153692526e7e66c2f2804cf24710ef08c997bf7
Julia310 <JPeters92@protonmail.com>
2024-06-10 16:53:54 +0200

    enable data processing via callback function, allow flexible data signatures and user defined chunk index lists

--- mltools/ml4xcube/datasets/tensorflow_xr.py
+++ mltools/ml4xcube/datasets/tensorflow_xr.py
@@ -1,34 +1,41 @@
 import random
 import xarray as xr
 import tensorflow as tf
-
-from ml4xcube.cube_utilities import get_chunk_by_index, calculate_total_chunks
 from ml4xcube.preprocessing import apply_filter, drop_nan_values
+from ml4xcube.cube_utilities import get_chunk_by_index, calculate_total_chunks
+
 
 class LargeScaleXrDataset:
-    def __init__(self, xr_dataset: xr.Dataset, num_chunks: int, rand_chunk: bool = True,
-                 drop_nan: bool = True, filter_var: str = 'land_mask'):
+    def __init__(self, xr_dataset: xr.Dataset, chunk_indices: list = None, num_chunks: int = None,
+                 rand_chunk: bool = True, drop_nan: bool = True, filter_var: str = 'land_mask', callback_fn=None):
         """
         Initialize the dataset for TensorFlow, managing large datasets efficiently.
 
         Args:
             xr_dataset (xr.Dataset): The xarray dataset.
+            chunk_indices (list): List of indices of chunks to load.
             num_chunks (int): Number of chunks to process dynamically.
             rand_chunk (bool): Whether to select chunks randomly.
             drop_nan (bool): Whether to drop NaN values.
             filter_var (str): Filtering variable name, default 'land_mask'.
         """
+        self.chunk_indices = None
         self.ds = xr_dataset
         self.num_chunks = num_chunks
         self.rand_chunk = rand_chunk
+        self.callback_fn = callback_fn
         self.drop_nan = drop_nan
         self.filter_var = filter_var
         self.total_chunks = calculate_total_chunks(xr_dataset)
-        if self.total_chunks >= num_chunks:
+        if not chunk_indices is None:
+            self.chunk_indices = chunk_indices
+        elif num_chunks is not None and self.total_chunks >= num_chunks:
             self.chunk_indices = random.sample(range(self.total_chunks), num_chunks)
         else:
             self.chunk_indices = list(range(self.total_chunks))
 
+    def __len__(self):
+        return len(self.chunk_indices)
 
     def generate(self):
         """
@@ -48,6 +55,9 @@ class LargeScaleXrDataset:
             if self.drop_nan:
                 cft = drop_nan_values(cft, list(cft.keys()))
 
+            if self.callback_fn:
+                cft = self.callback_fn(cft)
+
             yield cft
 
     def get_dataset(self):
@@ -57,11 +67,22 @@ class LargeScaleXrDataset:
         Returns:
             tf.data.Dataset: The TensorFlow Dataset object.
         """
+        example_chunk = next(self.generate())
+        if self.callback_fn is None:
+            output_signature = {name: tf.TensorSpec(shape=(None,), dtype=tf.float32)
+                                for name in example_chunk.keys()}
+        else:
+            output_signature = (
+                tf.TensorSpec(shape=(None,None), dtype=tf.float32),
+                tf.TensorSpec(shape=(None,None), dtype=tf.float32)
+            )
+
         return tf.data.Dataset.from_generator(
             self.generate,
-            output_signature={name: tf.TensorSpec(shape=(None,), dtype=tf.float32)
-                              for name in next(self.generate()).keys()}  # Assumes all data are float32
+            output_signature=output_signature
         )
+
+
 
 
 def prepare_dataset(dataset: tf.data.Dataset, batch_size: int, shuffle: bool = True, num_parallel_calls: int = None, distributed: bool = False) -> tf.data.Dataset:
