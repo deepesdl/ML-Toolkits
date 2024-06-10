9b37cd1b02a9e09881b7076e3a656fea11d1a5a7
Julia310 <JPeters92@protonmail.com>
2024-06-10 16:35:35 +0200

    enable user defined steps per epoch/validation

--- mltools/ml4xcube/training/tensorflow.py
+++ mltools/ml4xcube/training/tensorflow.py
@@ -17,12 +17,13 @@ class Trainer:
             loss: Union[tf.keras.losses.Loss, str] = "mean_squared_error",
             early_stopping: bool = True,
             patience: int = 10,
-            metrics: list = ['mae'],
+            metrics: list = None,
             tf_log_dir: str = './logs',
             mlflow_run=None,
             summery: bool = False,
             epochs: int = 100,
-            batch_size: int = 32,
+            train_epoch_steps = None,
+            val_epoch_steps = None
     ) -> None:
         self.model = model
         self.train_data = train_data
@@ -32,21 +33,29 @@ class Trainer:
         self.best_model_path = best_model_path
         self.loss = loss
         self.max_epochs = epochs
-        self.batch_size = batch_size
         self.early_stopping = early_stopping
         self.patience = patience
         self.metrics = metrics
         self.tf_log_dir = tf_log_dir
         self.mlflow_run = mlflow_run
         self.summery = summery
+        self.steps_per_train_epoch = train_epoch_steps
+        self.steps_per_validation_epoch = val_epoch_steps
 
     def train(self):
 
         """
         The main training loop with specific steps per epoch for training and validation.
         """
-        steps_per_train_epoch = len(self.train_data) // self.batch_size
-        steps_per_validation_epoch = len(self.test_data) // self.batch_size
+        if self.steps_per_train_epoch is None:
+            for batch in self.train_data.take(1):
+                batch_size = batch[0].shape[0]
+            self.steps_per_train_epoch = len(self.train_data) // batch_size
+
+        if self.steps_per_validation_epoch is None:
+            for batch in self.test_data.take(1):
+                batch_size = batch[0].shape[0]
+            self.steps_per_validation_epoch = len(self.test_data) // batch_size
 
         callbacks = [tf.keras.callbacks.TensorBoard(log_dir=self.tf_log_dir, histogram_freq=1)]
 
@@ -74,11 +83,11 @@ class Trainer:
         self.model.optimizer.learning_rate.assign(self.learning_rate)
 
         self.model.fit(
-            self.train_data.batch(self.batch_size),
+            self.train_data,
             epochs=self.max_epochs,
-            steps_per_epoch=steps_per_train_epoch,
-            validation_data=self.test_data.batch(self.batch_size),
-            validation_steps=steps_per_validation_epoch,
+            steps_per_epoch=self.steps_per_train_epoch,
+            validation_data=self.test_data,
+            validation_steps=self.steps_per_validation_epoch,
             callbacks=callbacks
         )
 
@@ -86,5 +95,4 @@ class Trainer:
             self.mlflow_run.log_artifact(self.best_model_path, "model")
 
         return self.model
-
 
