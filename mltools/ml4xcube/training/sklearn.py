c8791ecfba1e365dc9fb16167974394585eb08cf
Julia310 <JPeters92@protonmail.com>
2024-06-10 16:38:01 +0200

    enable unsupervised training

--- mltools/ml4xcube/training/sklearn.py
+++ mltools/ml4xcube/training/sklearn.py
@@ -23,11 +23,12 @@ class Trainer:
             self,
             model,
             train_data,
-            test_data,
+            test_data = None,
             metrics: list = [mean_squared_error],
             model_path: str = None,
-            batch_training: bool = True,
-            mlflow_run=None
+            batch_training: bool = False,
+            mlflow_run = None,
+            task_type = 'supervised'
     ) -> None:
         self.model = model
         self.train_data = train_data
@@ -36,6 +37,7 @@ class Trainer:
         self.model_path = model_path
         self.batch_training = batch_training
         self.mlflow_run = mlflow_run
+        self.task_type = task_type
 
     def run_batch_training(self, mode='Training'):
         """Validates the model on the test dataset if provided."""
@@ -43,18 +45,30 @@ class Trainer:
             total_score = 0
             count = 0
             if mode == 'Training':
-                for inputs, targets in self.train_data:
-                    inputs = inputs.numpy()
-                    targets = targets.numpy()
-                    self.model.partial_fit(inputs, targets)
+                for batch in self.train_data:
+                    if self.task_type == 'supervised':
+                        inputs, targets = batch
+                        inputs = inputs.numpy()
+                        targets = targets.numpy()
+                        self.model.partial_fit(inputs, targets)
+                    else:
+                        batch = batch.numpy()
+                        self.model.partial_fit(batch)
+
                 data_loader = self.train_data
             else:
                 data_loader = self.test_data
 
-            for inputs, targets in data_loader:
-                inputs = inputs.numpy()
-                targets = targets.numpy()
-                score = self.metrics[0](targets, self.model.predict(inputs))
+            for batch in data_loader:
+                if self.task_type == 'supervised':
+                    inputs, targets = batch
+                    inputs = inputs.numpy()
+                    targets = targets.numpy()
+                    score = self.metrics[0](targets, self.model.predict(inputs))
+
+                else:
+                    inputs = batch.numpy()
+                    score = self.metrics[0](inputs, self.model.predict(inputs))
                 total_score += score * len(inputs)
                 count += len(inputs)
             avg_score = total_score / count
@@ -64,21 +78,35 @@ class Trainer:
 
     def train(self):
         if not self.batch_training:
-            X_train, y_train = self.train_data
-            X_test, y_test = self.train_data
-            self.model.fit(X_train, y_train)
-            avg_train_score = self.metrics[0](y_train, self.model.predict(X_train))
-            print(f"Training Score: {avg_train_score:.4f}")
-            avg_val_score = self.metrics[0](y_test, self.model.predict(X_test))
-            print(f"Validation Score: {avg_val_score:.4f}")
+            if self.task_type == 'supervised':
+                X_train, y_train = self.train_data
+                X_test, y_test = self.test_data
+                self.model.fit(X_train, y_train)
+                train_score = self.metrics[0](y_train, self.model.predict(X_train))
+                print(f"Training Score: {train_score:.4f}")
+                val_score = self.metrics[0](y_test, self.model.predict(X_test))
+                print(f"Validation Score: {val_score:.4f}")
+            else:
+                X_train = self.train_data
+                self.model.fit(X_train)
+
+                # For unsupervised tasks, we evaluate using the training data itself
+                train_pred = self.model.predict(X_train)
+
+                # Calculating the scores based on available metrics
+                train_score = self.metrics[0](X_train, train_pred)
+                print(f"Score: {train_score:.4f}")
+
         else:
-            avg_train_score = self.run_batch_training(mode='Training')
-            avg_val_score = self.run_batch_training(mode='Validation')
+            train_score = self.run_batch_training(mode='Training')
+            if self.task_type == 'supervised':
+                val_score = self.run_batch_training(mode='Validation')
 
         if self.mlflow_run:
             # Logging metrics
-            self.mlflow_run.log_metric("training_score", avg_train_score)
-            self.mlflow_run.log_metric("validation_score", avg_val_score)
+            self.mlflow_run.log_metric("training_score", train_score)
+            if self.task_type == 'supervised':
+                self.mlflow_run.log_metric("validation_score", val_score)
 
         if self.model_path:
             # Pickling the model to save
