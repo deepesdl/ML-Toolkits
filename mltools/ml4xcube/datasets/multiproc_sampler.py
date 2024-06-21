39a5e022aafd9caa350a9e8baec8e35aa14117ec
Julia310 <JPeters92@protonmail.com>
2024-06-21 19:56:36 +0200

    update update sampler to return xarray instead of zarr

--- mltools/ml4xcube/datasets/multiproc_sampler.py
+++ mltools/ml4xcube/datasets/multiproc_sampler.py
@@ -3,6 +3,7 @@ import math
 import random
 import numpy as np
 import xarray as xr
+import dask.array as da
 from multiprocessing import Pool
 from typing import Tuple, Optional, Dict, List
 from ml4xcube.cube_utilities import split_chunk
@@ -43,9 +44,10 @@ def worker_preprocess_chunk(args):
 
 
 class MultiProcSampler():
-    def __init__(self, ds: xr.Dataset, filter_var: str = 'land_mask',
-                 data_fraq: float = 0.03, data_split: float = 0.8,
-                 nproc: int = 4, callback_fn = None,
+    def __init__(self, ds: xr.Dataset, filter_var: str = 'land_mask', chunk_size: Tuple = (1, ),
+                 nproc: int = 4, data_fraq: float = 1.0, rand_chunk: bool = False,
+                 array_dims: Tuple[str, Optional[str], Optional[str]] = ('samples',),
+                 data_split: float = 0.8, chunk_batch: int = None, callback_fn = None,
                  block_sizes: Optional[Dict[str, Optional[int]]] = None,
                  point_indices: Optional[List[Tuple[str, int]]] = None,
                  overlap: Optional[List[Tuple[str, int]]] = None):
@@ -57,6 +59,7 @@ class MultiProcSampler():
             filter_var (str): The variable to use for filtering. Defaults to 'land_mask'.
                                If None, no filtering is applied.
             data_fraq (float): The fraction of data to process.
+            rand_chunk (bool): Whether to select chunks randomly.
             data_split (float): The fraction of data to use for training. The rest is used for testing.
             nproc (int): Number of processes to use for parallel processing.
             callback_fn (function): Optional callback function to apply to each chunk after preprocessing.
@@ -74,13 +77,18 @@ class MultiProcSampler():
         self.block_sizes = block_sizes
         self.point_indices = point_indices
         self.overlap = overlap
-        self.total_chunks = int(calculate_total_chunks(self.ds, self.block_sizes) * self.data_fraq)
+        self.total_chunks = int(calculate_total_chunks(self.ds, self.block_sizes))
+        self.num_chunks = int(self.total_chunks * self.data_fraq)
         self.chunks = None
-        self.nproc = nproc
+        self.rand_chunk = rand_chunk
+        self.nproc = min(nproc, self.num_chunks)
+        self.array_dims = array_dims
+        self.chunk_batch = chunk_batch if chunk_batch is not None else self.nproc
         self.callback_fn = callback_fn
         self.train_store = zarr.open('train_cube.zarr')
         self.test_store = zarr.open('test_cube.zarr')
-        self.chunk_size = tuple(dim[1] for dim in self.point_indices) if self.point_indices else (1000,)
+        self.chunk_size = tuple(dim[1] for dim in self.point_indices) if self.point_indices else chunk_size
+        self.total_chunks = calculate_total_chunks(ds)
         self.create_cubes()
 
     def store_chunks(self, processed_chunks):
@@ -95,7 +103,6 @@ class MultiProcSampler():
             None
         """
         for chunk, train_indices, test_indices in processed_chunks:
-            print(train_indices, test_indices)
 
             if chunk is None:
                 continue
@@ -105,8 +112,8 @@ class MultiProcSampler():
                 test_data = chunk[var][test_indices]
 
                 print(f"Processing variable: {var}")
-                print(f"Train data shape: {train_data.shape}")
-                print(f"Test data shape: {test_data.shape}")
+                print(f"Train data samples: {train_data.shape[0]}")
+                print(f"Test data samples: {test_data.shape[0]}")
 
                 if var not in self.train_store:
                     self.train_store.create_dataset(var, data=train_data, chunks=self.chunk_size, append_dim=0)
@@ -124,22 +131,54 @@ class MultiProcSampler():
         Returns:
             None
         """
-        chunk_indices = list(range(self.total_chunks))
-        chunks = [get_chunk_by_index(self.ds, idx) for idx in chunk_indices]
+        if self.rand_chunk:
+            chunk_indices = random.sample(range(self.total_chunks), self.num_chunks)
+        else:
+            chunk_indices = list(range(self.num_chunks))
+
         with Pool(processes=self.nproc) as pool:
-            for i in range(0, self.total_chunks, math.ceil(self.nproc*0.2)):
-                batch_chunks = chunks[i:i + math.ceil(self.nproc*0.2)]
+            for i in range(0, self.num_chunks, self.chunk_batch):
+                batch_indices = chunk_indices[i:i + self.chunk_batch]
+                batch_chunks = [get_chunk_by_index(self.ds, idx) for idx in batch_indices]
                 processed_chunks = pool.map(worker_preprocess_chunk, [
                     (chunk, self.point_indices, self.overlap, self.filter_var, self.callback_fn, self.data_split)
                     for chunk in batch_chunks
                 ])
                 self.store_chunks(processed_chunks)
 
-    def get_datasets(self):
+    def assign_dims(self, data: Dict[str, da.Array]) -> Dict[str, xr.DataArray]:
         """
-        Retrieve the training and testing Zarr datasets.
+        Assign dimension names to the Dask arrays and convert them to xarray DataArray.
+
+        Args:
+            data (Dict[str, da.Array]): Dictionary containing Dask arrays.
 
         Returns:
-            Tuple[zarr.Group, zarr.Group]: The training and testing Zarr datasets.
+            Dict[str, xr.DataArray]: Dictionary containing xarray DataArray with assigned dimensions.
         """
-        return self.train_store, self.test_store
+        dims = self.array_dims
+        result = {}
+        for var, dask_array in data.items():
+            if len(dims) >= dask_array.ndim:
+                result[var] = xr.DataArray(dask_array, dims=dims[:dask_array.ndim])
+        return result
+
+    def get_datasets(self) -> Tuple[xr.Dataset, xr.Dataset]:
+        """
+        Retrieve the training and testing datasets as xarray Datasets.
+
+        Returns:
+            Tuple[xr.Dataset, xr.Dataset]: The training and testing xarray Datasets.
+        """
+        # Convert Zarr stores to Dask arrays and then to xarray Datasets
+        self.train_data = {var: da.from_zarr(self.train_store[var]) for var in self.train_store.array_keys()}
+        self.test_data = {var: da.from_zarr(self.test_store[var]) for var in self.test_store.array_keys()}
+
+        # Assign dimensions using the assign_dims function
+        self.train_data = self.assign_dims(self.train_data)
+        self.test_data = self.assign_dims(self.test_data)
+
+        train_ds = xr.Dataset(self.train_data)
+        test_ds = xr.Dataset(self.test_data)
+
+        return train_ds, test_ds
