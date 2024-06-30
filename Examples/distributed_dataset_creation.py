49c929230e3830eb7b7b8541063837e31c85d756
Julia310 <JPeters92@protonmail.com>
2024-06-30 22:04:43 +0200

    assign mean and std before standardizing

--- Examples/distributed_dataset_creation.py
+++ Examples/distributed_dataset_creation.py
@@ -8,34 +8,49 @@ from ml4xcube.preprocessing import get_statistics, standardize
 from ml4xcube.datasets.multiproc_sampler import MultiProcSampler
 
 
-at_stat, lst_stat = None, None
+# Initialize global variables to None
+at_stat = None
+lst_stat = None
 
 
-def standardize_data(chunk: xr.Dataset):
+def standardize_data(chunk: xr.Dataset) -> xr.Dataset:
     """
     Standardize xarray chunks.
+
+    Args:
+        chunk (xr.Dataset): The data chunk to be standardized.
+
+    Returns:
+        xr.Dataset: The standardized data chunk.
     """
     global at_stat, lst_stat
-
-    chunk['air_temperature_2m'] = standardize(chunk['air_temperature_2m'], *at_stat)
-    chunk['air_temperature_2m'] = standardize(chunk['land_surface_temperature'], *lst_stat)
+    print(at_stat)
+    print(lst_stat)
+    chunk['air_temperature_2m']       = standardize(chunk['air_temperature_2m'], *at_stat)
+    chunk['land_surface_temperature'] = standardize(chunk['land_surface_temperature'], *lst_stat)
 
     return chunk
 
 
-def create_datasets():
+def prepare_dataset_creation() -> xr.Dataset:
     """
-    Create dataset for distributed training
+    Prepare the dataset for creation by fetching the data, calculating statistics, and assigning a land mask.
+
+    Returns:
+        xr.Dataset: The prepared dataset.
     """
     data_store = new_data_store("s3", root="esdl-esdc-v2.1.1", storage_options=dict(anon=True))
     dataset    = data_store.open_data('esdc-8d-0.083deg-184x270x270-2.1.1.zarr')
     start_time = "2002-05-21"
     end_time   = "2002-08-01"
-    ds         = dataset[["land_surface_temperature", "air_temperature_2m"]].sel(time=slice(start_time, end_time))
+    ds = dataset[["land_surface_temperature", "air_temperature_2m"]].sel(time=slice(start_time, end_time))
 
     global at_stat, lst_stat
     at_stat  = get_statistics(ds, 'air_temperature_2m')
     lst_stat = get_statistics(ds, 'land_surface_temperature')
+    print('Compute parameters for standardization:')
+    print(f'air_temperature_2m:       {at_stat}')
+    print(f'land_surface_temperature: {lst_stat}')
 
     # Create a land mask
     lon_grid, lat_grid = np.meshgrid(ds.lon, ds.lat)
@@ -44,12 +59,21 @@ def create_datasets():
 
     # Assign land mask to the dataset
     ds = ds.assign(land_mask=(['time', 'lat', 'lon'], lm.rechunk(chunks=([v for _, v in get_chunk_sizes(ds)]))))
+    return ds
 
+
+def create_datasets(ds: xr.Dataset) -> None:
+    """
+    Create dataset for distributed training.
+
+    Args:
+        ds (xr.Dataset): The prepared dataset.
+    """
     # Preprocess data and split into training and testing sets
     train_set, test_set = MultiProcSampler(
         ds          = ds,
-        train_cube  = 'train_cube.zarr',
-        test_cube   = 'test_cube.zarr',
+        train_cube  = 'train_cube_std.zarr',
+        test_cube   = 'test_cube_std.zarr',
         nproc       = 5,
         chunk_batch = 10,
         data_fraq   = 0.01,
@@ -57,8 +81,12 @@ def create_datasets():
     ).get_datasets()
 
 
-def main():
-    create_datasets()
+def main() -> None:
+    """
+    Main function to prepare the dataset and create training/testing datasets.
+    """
+    ds = prepare_dataset_creation()
+    create_datasets(ds)
 
 
 if __name__ == "__main__":
