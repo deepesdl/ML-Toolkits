f8ee97d0142ff2956c0e4ccdf48e6ce420149ed0
Julia310 <JPeters92@protonmail.com>
2024-07-03 14:11:28 +0200

    enable provision of multiple validation metrics

--- mltools/ml4xcube/training/pytorch.py
+++ mltools/ml4xcube/training/pytorch.py
@@ -1,5 +1,6 @@
 import os
 import torch
+from typing import Callable, Dict
 from torch.utils.data import DataLoader
 from ml4xcube.training.train_plots import plot_loss
 
@@ -17,8 +18,8 @@ class Trainer:
             best_model_path: str,
             early_stopping: bool = True,
             patience: int = 10,
-            loss = torch.nn.MSELoss(reduction='mean'),
-            metrics: list = None,
+            loss: Callable = None,
+            metrics: Dict[str, Callable] = None,
             epochs: int = 10,
             mlflow_run=None,
             device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
@@ -34,13 +35,13 @@ class Trainer:
             optimizer (torch.optim.Optimizer): Optimizer for training.
             best_model_path (str): Path to save the best model.
             early_stopping (bool): Enable or disable early stopping. Defaults to True.
-            patience (int): Number of epochs to wait for improvement before stopping early. Defaults to 10.
-            loss (torch.nn.Module): Loss function. Defaults to MSELoss.
-            metrics (list): List of metrics to evaluate. Defaults to None.
-            epochs (int): Number of training epochs. Defaults to 10.
-            mlflow_run: An MLflow run instance to log training and validation metrics. Defaults to None.
-            device (torch.device): Device to run the training on. Defaults to CUDA if available.
-            create_loss_plot (bool): Whether to create a plot of training and validation loss. Defaults to False.
+            patience (int): Number of epochs to wait for improvement before stopping early.
+            loss (torch.nn.Module): Loss function.
+            metrics (Dict[str, Callable]): Dictionary of metrics to evaluate, with metric names as keys and metric functions as values.
+            epochs (int): Number of training epochs.
+            mlflow_run: An MLflow run instance to log training and validation metrics.
+            device (torch.device): Device to run the training on.
+            create_loss_plot (bool): Whether to create a plot of training and validation loss.
         """
         self.model = model.to(device)
         self.train_data = train_data
@@ -118,6 +119,9 @@ class Trainer:
         self.model.eval()
         total_loss = 0.0
         total_count = 0
+        metric_sums = {}
+        if self.metrics is not None:
+            metric_sums = {name: 0.0 for name in self.metrics.keys()}
         for batch in self.test_data:
             inputs, targets = batch
             if inputs.numel() == 0: continue
@@ -127,8 +131,19 @@ class Trainer:
                 total_loss += loss.item() * len(inputs)
                 total_count += len(inputs)
 
+                if self.metrics is not None:
+                    for name, metric in self.metrics.items():
+                        metric_value = metric(outputs, targets).item()
+                        metric_sums[name] += metric_value * len(inputs)
+
         avg_val_loss = total_loss / total_count
         print(f"Epoch {epoch + 1}: Validation Loss: {avg_val_loss:.4e}")
+
+        if self.metrics is not None:
+            avg_metrics = {name: total / total_count for name, total in metric_sums.items()}
+            for name, value in avg_metrics.items():
+                print(f"Epoch {epoch + 1}: {name}: {value:.4e}")
+
         if self.mlflow_run:
             self.mlflow_run.log_metric("validation_loss", avg_val_loss, step=epoch)
 
@@ -154,7 +169,7 @@ class Trainer:
             else:
                 self.strikes += 1
 
-            if self.early_stopping and self.strikes > self.patience:
+            if self.early_stopping and self.strikes >= self.patience:
                 print('Stopping early due to no improvement.')
                 break
 
