edfea5516e9965498db6de0912005d0445678a6c
Julia310 <JPeters92@protonmail.com>
2024-07-06 20:45:14 +0200

    increase flexibilty, enable assignment of layer dimensions

--- mltools/ml4xcube/cube_insights.py
+++ mltools/ml4xcube/cube_insights.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import xarray as xr
 from tqdm import tqdm
+from ml4xcube.cube_utilities import get_dim_range
 
 """
 Function get_insights(cube):
@@ -34,53 +35,46 @@ Example:
 
 
 # Function to get insights from the data cube
-def get_insights(cube: xr.DataArray) -> None:
+def get_insights(cube: xr.DataArray, variable: str, layer_dim: str = None) -> None:
     """
     Extracts and prints various characteristics of the data cube.
 
     Args:
         cube (xr.DataArray): The input data cube.
+        variable (str): The variable name to extract from the data cube.
+        layer_dim (str, optional): The dimension along which to iterate (e.g., 'time'). Defaults to None.
 
     Returns:
         None
     """
-    # Extracting the variable's long name
-    variable = cube.long_name
-
     # Extracting the dimension names
-    dim1 = cube.dims[0]
-    dim2 = cube.dims[1]
-    dim3 = cube.dims[2]
+    dims = list(cube.dims)
+    dim1, dim2, dim3 = dims[0], dims[1], dims[2]
+    if layer_dim is None: layer_dim = dim1
+
+    layer_coords = [s for s in dims if s != layer_dim]
+    layer_coord1, layer_coord2 = layer_coords
+
+    #Extracting the shape
+    sizes = cube.sizes
+    size_values = list(sizes.values())
 
     # Extracting the dimension long names
     dim1_long_name = cube[dim1].long_name
     dim2_long_name = cube[dim2].long_name
     dim3_long_name = cube[dim3].long_name
 
-    # Extracting the range of dim1 (e.g. time), dim2 and dim3 and rounding them to 3 decimal places (e.g. lat and lon)
-    try:
-        dim1_min = str(np.datetime_as_string(cube[dim1][0], unit='D'))
-        dim1_max = str(np.datetime_as_string(cube[dim1][-1], unit='D'))
-    except:
-        dim1_min = str(cube[dim1][0])
-        dim1_max = str(cube[dim1][-1])
-    try:
-        dim2_min = round(cube[dim2].values.min(), 3)
-        dim2_max = round(cube[dim2].values.max(), 3)
-    except:
-        dim2_min = str(cube[dim2][0])
-        dim2_max = str(cube[dim2][-1])
-    try:
-        dim3_min = round(cube[dim3].values.min(), 3)
-        dim3_max = round(cube[dim3].values.max(), 3)
-    except:
-        dim3_min = str(cube[dim3][0])
-        dim3_max = str(cube[dim3][-1])
+
+    dim1_min, dim1_max = get_dim_range(cube, dim1)
+    dim2_min, dim2_max = get_dim_range(cube, dim2)
+    dim3_min, dim3_max = get_dim_range(cube, dim3)
 
 
     # Calculating the size of the data cube and each layer
-    cube_size = cube.size
-    layer_size = cube[0].size
+    cube_size = size_values[0]*size_values[1]*size_values[2]
+    layer_size = sizes[layer_coord1] * sizes[layer_coord2]
+
+    cube = cube[variable]
 
     # Calculating the number and percentage of NaN values (gaps) in the entire cube
     gap_values = np.sum(np.isnan(cube)).values.item()
@@ -91,12 +85,13 @@ def get_insights(cube: xr.DataArray) -> None:
     max_value = np.nanmax(cube)
 
     # Initializing dictionaries to track the maximum and minimum gap sizes and their dates
-    max_gap = {"gap_size": 0, "date": None}
-    min_gap = {"gap_size": 1, "date": None}
+    max_gap = {"gap_size": 0, "dim_val": None}
+    min_gap = {"gap_size": 1, "dim_val": None}
 
     # Looping through each (time) layer in the cube
     with tqdm(total=len(cube), file=sys.stdout, colour='GREEN', bar_format='{l_bar}{bar:20}{r_bar}') as pbar:
-        for c in cube:
+        for i in range(cube.sizes[layer_dim]):
+            c = cube.isel({layer_dim: i})
             # Calculating the absolute and relative gap size for the current layer
             c_gap_absolute = np.sum(np.isnan(c)).values.item()
             c_gap_size = round(c_gap_absolute / layer_size, 2)
@@ -104,14 +99,14 @@ def get_insights(cube: xr.DataArray) -> None:
             # Updating max_gap and min_gap if the current layer's gap size is greater or smaller respectively
             if c_gap_size > max_gap["gap_size"]:
                 try:
-                    max_gap = {"gap_size": c_gap_size, "date": str(np.datetime_as_string(c.time.values, unit='D'))}
+                    max_gap = {"gap_size": c_gap_size, "dim_val": str(np.datetime_as_string(c.time.values, unit='D'))}
                 except:
-                    max_gap = {"gap_size": c_gap_size, "date": str(c.time.values)}
+                    max_gap = {"gap_size": c_gap_size, "dim_val": str(c.time.values)}
             if c_gap_size < min_gap["gap_size"]:
                 try:
-                    min_gap = {"gap_size": c_gap_size, "date": str(np.datetime_as_string(c.time.values, unit='D'))}
+                    min_gap = {"gap_size": c_gap_size, "dim_val": str(np.datetime_as_string(c.time.values, unit='D'))}
                 except:
-                    min_gap = {"gap_size": c_gap_size, "date": str(c.time.values)}
+                    min_gap = {"gap_size": c_gap_size, "dim_val": str(c.time.values)}
 
             # Update the progress bar
             pbar.update(1)
@@ -120,30 +115,30 @@ def get_insights(cube: xr.DataArray) -> None:
     print("The data cube has the following characteristics:")
     print(" ")
     print(f"{'Variable:':<25} {variable}")
-    print(f"{'Shape:':<25} ({dim1}: {cube.shape[0]}, {dim2}: {cube.shape[1]}, {dim3}: {cube.shape[2]})")
+    print(f"{'Shape:':<25} ({dim1}: {size_values[0]}, {dim2}: {size_values[1]}, {dim3}: {size_values[2]})")
     print(f"{dim1_long_name + ' range:':<25} {dim1_min} - {dim1_max}")
     print(f"{dim2_long_name + ' range:':<25} {dim2_min} - {dim2_max}")
     print(f"{dim3_long_name + ' range:':<25} {dim3_min} - {dim3_max}")
     print(f"{'Total size:':<25} {cube_size:,}")
     print(f"{'Size of each layer:':<25} {layer_size:,}")
     print(f"{'Total gap size:':<25} {gap_values} -> {int(gap_values_percentage * 100)} %")
-    print(f"{'Maximum gap size:':<25} {int(max_gap['gap_size'] * 100)} % on {max_gap['date']}")
-    print(f"{'Minimum gap size:':<25} {int(min_gap['gap_size'] * 100)} % on {min_gap['date']}")
+    print(f"{'Maximum gap size:':<25} {int(max_gap['gap_size'] * 100)} % on {max_gap['dim_val']}")
+    print(f"{'Minimum gap size:':<25} {int(min_gap['gap_size'] * 100)} % on {min_gap['dim_val']}")
     print(f"{'Value range:':<25} {min_value:.2f} - {max_value:.2f}")
 
 
 # Function to generate a heat map of gap (NaN) counts for each lat/lon pixel (or other dimensions)
-def get_gap_heat_map(cube: xr.DataArray) -> xr.DataArray:
+def get_gap_heat_map(cube: xr.DataArray, count_dim: str) -> xr.DataArray:
     """
-    Generates a heat map of value counts (non-NaN values) for each latitude/longitude pixel (or other dimensions).
+    Generates a heat map of value counts (non-NaN values) for each latitude/longitude pixel.
 
     Args:
         cube (xr.DataArray): The input data cube.
+        count_dim (str): The name of the dimension along which to count NaN values.
 
     Returns:
-        xr.DataArray: Heat map of non-NaN value counts for each lat/lon pixel (or other dimensions).
+        xr.DataArray: Heat map of non-NaN value counts for each lat/lon pixel.
     """
-    # Count the number of non-NaN values for each lat/lon pixel (or other dimensions)
-    dim1 = cube.dims[0]
-    nan_counts = cube.shape[0] - np.isnan(cube).sum(dim=dim1)
+    # Count the number of non-NaN values for each lat/lon pixel
+    nan_counts = cube.shape[0] - np.isnan(cube).sum(dim=count_dim)
     return nan_counts
