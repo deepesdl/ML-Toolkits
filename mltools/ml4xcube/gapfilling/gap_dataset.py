cfc13e5f9041e19250e84fa56220aad829a6c709
Julia310 <JPeters92@protonmail.com>
2024-07-06 21:06:19 +0200

    use predictor path to calculate extra matrix, enable costum layer dimension definition

--- mltools/ml4xcube/gapfilling/gap_dataset.py
+++ mltools/ml4xcube/gapfilling/gap_dataset.py
@@ -5,54 +5,29 @@ import shutil
 import datetime
 import numpy as np
 import xarray as xr
-from typing import Dict, List, Union, Optional
+from typing import Dict, List, Union
 
 """
 In this file we prepare the data for the gapfilling algorithm. 
 We slice a specific dimensions (e.g. area and time range) from the data cube with the values for the specified variable.
 If requested artificial gaps can be inserted in the array. 
-
-Example
-```
-    from xcube.core.store import new_data_store
-    
-    # Directory name
-    ds_name = 'GermanyNB123'
-    variable = 'land_surface_temperature'
-    dimensions = {
-        'lat': (54, 48),
-        'lon': (6, 15),
-        'time': (datetime.date(2008, 1, 1), datetime.date(2008, 12, 31))
-    }
-    artificial_gaps = None
-    actual_matrix = datetime.date(2008, 9, 1)
-    data_store = new_data_store("s3", root="esdl-esdc-v2.1.1", storage_options=dict(anon=True))
-    dataset = data_store.open_data('esdc-8d-0.083deg-184x270x270-2.1.1.zarr')
-    ds = dataset[variable]
-    
-    path = os.path.dirname(os.getcwd()) + "GermanyNB123.zarr"
-    
-    ds.to_zarr(path, mode="w")
-    ds = xr.open_zarr(path)[variable]
-    
-    GapDataset(ds, ds_name, dimensions, artificial_gaps, actual_matrix).get_data()
-```
 """
 
 
 class GapDataset:
     def __init__(self, ds: xr.DataArray, ds_name: str = 'Test123',
-                 dimensions: Optional[Dict[str, tuple]] = None,
-                 artificial_gaps: Optional[List[float]] = None,
-                 actual_matrix: Union[str, datetime.date] = 'Random'):
+                 dimensions: Dict[str, tuple] = None,
+                 artificial_gaps: List[float] = None,
+                 actual_matrix: Union[str, datetime.date] = 'Random',
+                 predictor_path: str = None, layer_dim: str = 'time'):
         """
         Represents a dataset for handling data gaps.
 
         Attributes:
             ds (xr.DataArray): The original dataset that might be sliced later on.
             ds_name (str): The name of the dataset.
-            dimensions (Optional[Dict[str, tuple]]): Dict containing dimension ranges (e.g. lat, lon, times); no slicing if no dim specified.
-            artificial_gaps (Optional[List[float]]): List of artificial gap sizes; None if no artificial gaps should be created.
+            dimensions (Dict[str, tuple]): Dict containing dimension ranges (e.g. lat, lon, times); no slicing if no dim specified.
+            artificial_gaps (List[float]): List of artificial gap sizes; None if no artificial gaps should be created.
             actual_matrix (Union[str, datetime.date]): Specifies the actual data matrix or 'Random' for random selection.
             directory (str): The directory where data will be stored.
             extra_data (xr.DataArray): Additional data used as predictors (e.g. Land Cover Classes).
@@ -63,11 +38,12 @@ class GapDataset:
         self.dimensions = dimensions
         self.artificial_gaps = artificial_gaps
         self.actual_matrix = actual_matrix
-
+        self.predictor_path = predictor_path
         self.directory = os.path.dirname(os.getcwd()) + '/application_results/' + ds_name + "/" if \
             os.getcwd().split('/')[-1] != 'gapfilling' else 'application_results/' + ds_name + "/"
         self.extra_data = None
         self.sliced_ds = None
+        self.layer_dim = layer_dim
 
     def get_data(self) -> None:
         """
@@ -90,7 +66,8 @@ class GapDataset:
         # Slice the dimensions from a global dataset
         self.slice_dataset()
         # Retrieve other extra matrix to use them as predictors (e.g. land cover data)
-        self.get_extra_matrix()
+        if self.predictor_path is not None:
+            self.get_extra_matrix()
         # If requested create artificial data gaps which values will be estimated later on
         self.process_actual_matrix()
 
@@ -140,31 +117,25 @@ class GapDataset:
             None
         """
         # Open the predictor datasets from zarr with the global predictor data
-        # Via Looping through all files in the target directory that start with "global"
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        target_dir = os.path.join(current_dir, 'helper')
+        try:
+            data_file = os.path.join(self.predictor_path)
 
-        for filename in os.listdir(target_dir):
-            if filename.startswith('global') and filename.endswith('.zarr'):
-                try:
-                    data_file = os.path.join(target_dir, filename)
+            # Open the predictor dataset
+            predictor_dataset = xr.open_zarr(data_file)
+            predictor_variable = str(list(predictor_dataset.data_vars)[0])
+            predictor_dataset = predictor_dataset[predictor_variable]
 
-                    # Open the predictor dataset
-                    predictor_dataset = xr.open_zarr(data_file)
-                    predictor_variable = str(list(predictor_dataset.data_vars)[0])
-                    predictor_dataset = predictor_dataset[predictor_variable]
+            # Extracting the dimension names
+            dim1 = predictor_dataset.dims[0]
+            dim2 = predictor_dataset.dims[1]
+            # Select and slice the predictor data based on the specified dimension ranges (e.g. lat and lon)
+            self.extra_data = predictor_dataset.sel(**{dim1: slice(self.dimensions[dim1][0], self.dimensions[dim1][1]),
+                                                       dim2: slice(self.dimensions[dim2][0], self.dimensions[dim2][1])})
 
-                    # Extracting the dimension names
-                    dim1 = predictor_dataset.dims[0]
-                    dim2 = predictor_dataset.dims[1]
-                    # Select and slice the predictor data based on the specified dimension ranges (e.g. lat and lon)
-                    self.extra_data = predictor_dataset.sel(**{dim1: slice(self.dimensions[dim1][0], self.dimensions[dim1][1]),
-                                                               dim2: slice(self.dimensions[dim2][0], self.dimensions[dim2][1])})
-
-                    self.extra_data.to_zarr(self.directory + "extra_matrix_" + predictor_variable + ".zarr", mode="w")
-                    print(f"Extra predictor data matrix for has {predictor_variable} been created.")
-                except:
-                    print(f"No extra predictor data matrix for {predictor_variable} has been created. ")
+            self.extra_data.to_zarr(self.directory + "extra_matrix_" + predictor_variable + ".zarr", mode="w")
+            print(f"Extra predictor data matrix for has {predictor_variable} been created.")
+        except:
+            print(f"No extra predictor data matrix for {predictor_variable} has been created. ")
 
     def process_actual_matrix(self) -> None:
         """
@@ -177,7 +148,8 @@ class GapDataset:
             None
         """
         # Select the relevant/random layer (e.g. date) with the gaps to be filled and slice the corresponding array
-        dim1 = self.ds.dims[0]
+        dim1 = self.layer_dim
+        print(f'dim1: {self.ds.dims[0]}')
         dim1_values = self.ds.coords[dim1].values
         try:
             actual_value = np.random.choice(dim1_values) if self.actual_matrix == 'Random' else np.datetime64(
@@ -185,7 +157,7 @@ class GapDataset:
         except:
             actual_value = np.random.choice(dim1_values) if self.actual_matrix == 'Random' else self.actual_matrix
 
-        actual_matrix = self.ds.sel(**{dim1: actual_value})
+        actual_matrix = self.ds.sel(**{dim1: actual_value}, method='nearest')
 
         # Calculate the real gap size percentage and print it with the relevant value (e.g. date) to give insights
         real_gap_size = round(np.sum(np.isnan(actual_matrix)).values.item() / actual_matrix.size * 100)
