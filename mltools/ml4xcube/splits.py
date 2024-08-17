f2d1436aa53e4606a51c62d1d5c1a15b83ade834
Julia310 <JPeters92@protonmail.com>
2024-08-17 17:22:52 +0200

    rename data_split to splits

---
+++ mltools/ml4xcube/splits.py
@@ -0,0 +1,126 @@
+import random
+import warnings
+import numpy as np
+import xarray as xr
+import dask.array as da
+from typing import Tuple, List, Union, Dict
+from ml4xcube.utils import get_chunk_sizes
+warnings.filterwarnings('ignore')
+
+
+def assign_rand_split(ds: xr.Dataset, split: float = 0.8) -> xr.Dataset:
+    """
+    Assign random split using random sampling.
+
+    Args:
+        ds (xr.Dataset): The xarray train_ds to which the random split will be assigned.
+        split (float): The proportion of the train_ds to be used for training (default is 0.8).
+
+    Returns:
+        xr.Dataset: The train_ds with an additional 'split' variable indicating the random split.
+    """
+    seed = 32  # Consistent seed for reproducibility
+    random.seed(seed)
+
+    # Fetch the chunk sizes using a predefined method that returns a list of tuples (dimension, chunk size)
+    chunk_sizes = dict(get_chunk_sizes(ds))  # Assuming this returns [(dim, size), ...]
+
+    # Create a Dask array that generates random numbers for each data point, following the train_ds's chunking structure
+    random_split = da.random.random(size=tuple(ds.dims[dim] for dim in ds.dims),
+                                    chunks=tuple(chunk_sizes[dim] for dim in ds.dims if dim in chunk_sizes)) < split
+
+    # Convert boolean values to floats (1.0 for True, 0.0 for False)
+    random_split = random_split.astype(float)
+
+    # Assign this array to the train_ds with the variable name 'split'
+    return ds.assign(split=(list(ds.dims), random_split))
+
+
+def assign_block_split(ds: xr.Dataset, block_size: List[Tuple[str, int]] = None, split: float = 0.8) -> xr.Dataset:
+    """
+    Assign blocks of data to training or testing sets based on a specified split ratio.
+
+    This function uniquely assigns a block of data to either a training or testing set using a random process.
+    The random seed for the assignment is generated using a Cantor pairing function on the block's indices.
+
+    Args:
+        ds (xr.Dataset): The input train_ds.
+        block_size (List[Tuple[str, int]]): List of tuples specifying the dimensions and their respective sizes.
+                                            If None, chunk sizes are inferred from the train_ds.
+        split (float): The fraction of data to assign to the training set. The remainder will be assigned to the testing set.
+
+    Returns:
+        xr.Dataset: The input train_ds with an additional variable 'split' that indicates whether each block belongs to the training (True) or testing (False) set.
+    """
+    if block_size is None:
+        block_size = get_chunk_sizes(ds)
+
+    def role_dice(x):
+        return x + (random.random() < split)
+
+    def block_rand(x):
+        block_ind_array = da.zeros(
+            (list(x.sizes.values())), chunks=([v for k, v in block_size])
+        )
+        mapped = block_ind_array.map_blocks(role_dice)
+        return list(ds.dims), mapped
+
+    return ds.assign({"split": block_rand})
+
+
+def create_split(
+        data: Union[xr.Dataset, Dict[str, np.ndarray]], to_pred: Union[List[str], str] = None,
+        exclude_vars: List[str] = list(), feature_vars: List[str] = None, stack_axis: int = -1
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Create a train-test split for the given feature variables and target variables using the 'split' variable.
+
+    Args:
+        data (Union[xr.Dataset, Dict[str, np.ndarray]]): The xarray train_ds or dictionary of variables.
+        to_pred (Union[List[str], str]): List of target variable names.
+        exclude_vars (str): Variable names to exclude from the features.
+        feature_vars (List[str]): List of feature variable names. If None, will be determined automatically.
+        stack_axis (int): Axis along which to stack the feature and target variables (default is -1).
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: Training and testing sets for features and targets.
+    """
+    if isinstance(data, xr.Dataset):
+        data_vars = list(data.data_vars)
+        get_values = lambda var: data[var].values
+    elif isinstance(data, dict):
+        data_vars = list(data.keys())
+        get_values = lambda var: data[var]
+    else:
+        raise TypeError("Input data must be an xarray.Dataset or a dictionary.")
+
+    if isinstance(to_pred, str): to_pred = [to_pred]
+
+    for var in to_pred + ['split'] + exclude_vars:
+        if var not in data_vars:
+            raise ValueError(f"Variable '{var}' not found in the data.")
+
+    # Determine feature variables if not provided
+    if feature_vars is None:
+        feature_vars = [var for var in data_vars if
+                        var not in to_pred + ['split', 'filter_mask'] + exclude_vars]
+
+    # Stack feature variables along the specified axis
+    X = np.stack([get_values(var) for var in feature_vars], axis=stack_axis)
+
+    # Stack target variables along the specified axis
+    y = np.stack([get_values(var) for var in to_pred], axis=stack_axis)
+
+    split = get_values('split')
+
+    X_train = X[split == 1]
+    X_test = X[split == 0]
+    y_train = y[split == 1]
+    y_test = y[split == 0]
+
+    return X_train, X_test, y_train, y_test
+
+
+
+
+
