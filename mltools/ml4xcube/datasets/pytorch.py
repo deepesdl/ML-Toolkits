9bd11730309fe3a845208e8150cf5c7a911eae3e
Julia310 <JPeters92@protonmail.com>
2024-08-17 17:54:48 +0200

    relocate chunk processing, data loader preparation for train and test set simultaneously

--- mltools/ml4xcube/datasets/pytorch.py
+++ mltools/ml4xcube/datasets/pytorch.py
@@ -2,54 +2,56 @@ import torch
 import random
 import numpy as np
 import xarray as xr
-from ml4xcube.cube_utilities import split_chunk
 from torch.utils.data import Dataset, DataLoader
-from typing import Tuple, List, Callable, Dict
-from ml4xcube.cube_utilities import calculate_total_chunks, get_chunk_by_index
-from ml4xcube.preprocessing import apply_filter, drop_nan_values, fill_nan_values
+from typing import Tuple, List, Callable, Dict, Union
+from ml4xcube.datasets.chunk_processing import process_chunk
+from ml4xcube.utils import calculate_total_chunks, get_chunk_by_index
 
 
 class PTXrDataset(Dataset):
-    def __init__(self, ds: xr.Dataset, rand_chunk: bool = True, drop_nan: bool = True,
-                 chunk_indices: List[int] = None, drop_nan_masked: bool = False, apply_mask: bool = True,
-                 drop_sample: bool = False, fill_method: str = None, const: float = None,
-                 filter_var: str = 'land_mask', num_chunks: int = None,
-                 block_sizes: List[Tuple[str, int]] = None,
-                 sample_size: List[Tuple[str, int]] = None,
-                 overlap: List[Tuple[str, float]] = None):
+    def __init__(self, ds: xr.Dataset, rand_chunk: bool = True, drop_nan: str = 'auto', drop_sample: bool = False,
+                 chunk_indices: List[int] = None, apply_mask: bool = True, fill_method: str = None,
+                 const: float = None, filter_var: str = 'land_mask', num_chunks: int = None, callback = None,
+                 block_sizes: List[Tuple[str, int]] = None, sample_size: List[Tuple[str, int]] = None,
+                 overlap: List[Tuple[str, float]] = None, process_chunks: bool = False):
         """
         Initialize the dataset to manage large datasets efficiently.
 
         Attributes:
             ds (xr.Dataset): The xarray dataset.
             rand_chunk (bool): Whether to select chunks randomly.
-            drop_nan (bool): Whether to drop NaN values.
-            drop_nan_masked (bool): If true, NaN values are dropped using the mask specified by filter_var.
+            drop_nan (str): Defines the means by which areas with missing values are dropped
+                If 'auto', drop the entire sample if any NaN is contained.
+                If 'if_all_nan', drop the sample if entirely NaN.
+                If 'masked', drop the entire subarray if valid values according to mask are NaN.
             apply_mask (bool): If true, apply the filter based on the specified filter_var.
             drop_sample (bool): If true, drop the entire subarray if any value in the subarray does not belong to the mask (False).
             fill_method (str): Method to fill masked data, if any.
             const (float): Constant value to use for filling masked data, if needed.
             filter_var (str): Filtering variable name.
             num_chunks (int): Number of chunks to process dynamically.
+            callback (Callable): Function to apply to each chunk after preprocessing.
             block_sizes (List[Tuple[str, int]]): Block sizes for considered blocks (of (sub-)chunks).
             sample_size (List[Tuple[str, int]]): Sample size for chunk splitting.
             overlap (List[Tuple[str, float]]): Percentage for overlapping samples due to chunk splitting.
             total_chunks (int): Total number of chunks in the dataset.
             chunk_indices (List[int]): List of indices of chunks to load.
+            process chunk (bool): Defines whether chunk processing is necessary.
         """
         self.ds = ds
         self.rand_chunk = rand_chunk
         self.drop_nan = drop_nan
-        self.drop_nan_masked = drop_nan_masked
         self.apply_mask = apply_mask
         self.drop_sample = drop_sample
         self.fill_method = fill_method
         self.const = const
         self.filter_var = filter_var
         self.num_chunks = num_chunks
+        self.callback_fn = callback
         self.block_sizes = block_sizes
         self.sample_size = sample_size
         self.overlap = overlap
+        self.process_chunks = process_chunks
 
         self.total_chunks = int(calculate_total_chunks(ds, self.block_sizes))
         if not chunk_indices is None:
@@ -79,63 +81,66 @@ class PTXrDataset(Dataset):
             Dict[str, np.ndarray]: A dictionary containing the processed chunk.
         """
         chunk_index = self.chunk_indices[idx]
-
         chunk = get_chunk_by_index(self.ds, chunk_index)
 
-        # Process the chunk
-        cf = split_chunk(chunk, self.sample_size, overlap=self.overlap)
+        if self.process_chunks:
+            chunk, _ = process_chunk(chunk, self.apply_mask, self.drop_sample, self.filter_var, self.sample_size,
+                                     self.overlap, self.fill_method, self.const, self.drop_nan)
 
-        if self.apply_mask:
-            cft = apply_filter(cf, self.filter_var, self.drop_sample)
-        else:
-            cft = cf
+        if self.callback_fn:
+            chunk = self.callback_fn(chunk)
 
-        if self.drop_nan:
-            vars = list(cft.keys())
-            if self.drop_nan_masked:
-                cft = drop_nan_values(cft, vars, self.filter_var)
-            else:
-                cft = drop_nan_values(cft, vars)
-
-        valid_chunk = all(np.nan_to_num(cft[var]).sum() > 0 for var in cf)
-
-        if valid_chunk:
-            vars = [var for var in cft.keys() if var != 'split' and var != self.filter_var]
-            if self.fill_method is not None:
-                cft = fill_nan_values(cft, vars, self.fill_method, self.const)
-
-        return cft  # Return the processed chunk
+        return chunk  # Return the processed chunk
 
 
-def prepare_dataloader(dataset: Dataset, batch_size: int = 1, callback_fn: Callable = None, num_workers: int = 0, parallel: bool = False, shuffle = True, drop_last=True) -> DataLoader:
+def prep_dataloader(train_ds: Dataset, test_ds: Dataset = None, batch_size: int = 1, callback: Callable = None, num_workers: int = 0, parallel: bool = False, shuffle = True, drop_last=True) -> Union[DataLoader, Tuple[DataLoader, DataLoader]]:
     """
-    Prepares a DataLoader.
+    Prepares a DataLoader for training and optionally testing.
 
     Args:
-        dataset (Dataset): The PyTorch dataset from which to load the data.
+        train_ds (Dataset): The PyTorch dataset from which to load training data.
+        test_ds (Dataset, optional): The PyTorch dataset from which to load test data. If None, only the training DataLoader is returned. Defaults to None.
         batch_size (int): How many samples per batch to load. Defaults to 1.
-        callback_fn (Callable): A function used to collate data into batches. Defaults to None.
-        num_workers (int): How many subprocesses to use for data loading. Defaults to 0.
-        parallel (bool): Specifies if distributed training is performed. Defaults to False.
-        shuffle (bool): Whether to shuffle the data at every epoch. Defaults to True.
+        callback (Callable, optional): A function used to collate data into batches. Defaults to None.
+        num_workers (int): How many subprocesses to use for data loading. Defaults to 0 (single process).
+        parallel (bool): Specifies if distributed training is performed. When True, a DistributedSampler is used for training, and shuffling is disabled. Defaults to False.
+        shuffle (bool): Whether to shuffle the data at every epoch. Defaults to True. Automatically set to False when `parallel` is True.
         drop_last (bool): Whether to drop the last incomplete batch, if the dataset size is not divisible by the batch size. Defaults to True.
 
-    Returns:
-        DataLoader: A DataLoader object.
+        Returns:
+        Union[DataLoader, Tuple[DataLoader, DataLoader]]:
+            If `test_ds` is None: Returns a single DataLoader object for the training dataset.
+            If `test_ds` is provided: Returns a tuple containing two DataLoader objects, one for the training dataset and one for the test dataset.
+
     """
     sampler = None
     if parallel:
         from torch.utils.data.distributed import DistributedSampler
-        sampler = DistributedSampler(dataset)
+        sampler = DistributedSampler(train_ds)
         shuffle = False
 
-    return DataLoader(
-        dataset,
+    train_loader = DataLoader(
+        train_ds,
         batch_size=batch_size,
         num_workers=num_workers,
         pin_memory=True if torch.cuda.is_available() else False,  # Conditionally use pin_memory
         shuffle=shuffle,
-        collate_fn=callback_fn,
+        collate_fn=callback,
         sampler=sampler,
         drop_last=drop_last
     )
+
+    if test_ds is not None:
+        test_loader = DataLoader(
+            test_ds,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=True if torch.cuda.is_available() else False,  # Conditionally use pin_memory
+            shuffle=shuffle,
+            collate_fn=callback,
+            sampler=sampler,
+            drop_last=drop_last
+        )
+        return train_loader, test_loader
+
+    return train_loader
