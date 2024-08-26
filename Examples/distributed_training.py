c76e8e8a81cb18fa8c74dde66ca0e60c03cc0828
Julia310 <JPeters92@protonmail.com>
2024-08-26 16:56:49 +0200

    incorporate package changes

--- Examples/distributed_training.py
+++ Examples/distributed_training.py
@@ -1,21 +1,17 @@
 import torch
-import argparse
 import numpy as np
 import xarray as xr
-from ml4xcube.datasets.pytorch import PTXrDataset, prepare_dataloader
-from ml4xcube.training.pytorch_distributed import ddp_init, dist_train, Trainer
+from ml4xcube.datasets.pytorch import PTXrDataset, prep_dataloader
+from ml4xcube.training.pytorch_distributed import ddp_init, Trainer
 
 """
 Before performing distributed machine learning, run the distributed_datasets_creation.py in order to create the training and the test set.
 
 To utilize ml4xcube for distributed training, use the following command with torchrun to initiate the process:
 
-torchrun --standalone --nproc_per_node=<number_of_processes> distributed_training.py <epochs> --save every <epochs number> --batch_size <batch_size>
+torchrun --standalone --nproc_per_node=<number_of_processes> distributed_training.py
 
-Replace `<number_of_processes>` with the number of processes you wish to run per node,
-`<epochs>` with the total number of training epochs,
-<epochs number> with the recurring number of epochs to save the current snapshot of the model weigths,
-and <batch_size> with the number of data samples each node processes
+Replace <number_of_processes> with the number of processes you wish to run
 """
 
 
@@ -59,7 +55,7 @@ def load_train_objs():
     return train_ds, test_ds, model, optimizer, loss
 
 
-def main(save_every: int, total_epochs: int, batch_size: int, snapshot_path: str = "snapshot.pt", best_model_path: str = 'Best_Model.pt'):
+def main():
     """Main function to run distributed training process."""
 
     # Initialize distributed data parallel training
@@ -69,8 +65,7 @@ def main(save_every: int, total_epochs: int, batch_size: int, snapshot_path: str
     train_set, test_set, model, optimizer, loss = load_train_objs()
 
     # Prepare data loaders
-    train_loader = prepare_dataloader(train_set, batch_size, num_workers=5, parallel=True, callback_fn=map_function)
-    test_loader  = prepare_dataloader(test_set, batch_size, num_workers=5, parallel=True, callback_fn=map_function)
+    train_loader, test_loader = prep_dataloader(train_set, test_set, batch_size=32, num_workers=5, parallel=True, callback=map_function)
 
     # Initialize the trainer and start training
     trainer = Trainer(
@@ -78,23 +73,16 @@ def main(save_every: int, total_epochs: int, batch_size: int, snapshot_path: str
         train_data           = train_loader,
         test_data            = test_loader,
         optimizer            = optimizer,
-        save_every           = save_every,
-        best_model_path      = best_model_path,
+        best_model_path      = 'Best_Model.pt',
         early_stopping       = True,
-        snapshot_path        = snapshot_path,
         patience             = 3,
         loss                 = loss,
         validate_parallelism = True
     )
 
-    dist_train(trainer, total_epochs)
+    trainer.train()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='simple distributed training job')
-    parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
-    parser.add_argument('--save_every', default=10, type=int, help='How often to save a snapshot')
-    parser.add_argument('--batch_size', default=7, type=int, help='Input batch size on each device (default: 32)')
-    args = parser.parse_args()
 
-    main(args.save_every, args.total_epochs, args.batch_size)
+    main()
